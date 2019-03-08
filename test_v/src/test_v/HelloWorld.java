8195f2f8c4ef1f901ce0aa254a5766ba39227ccd
Kams <houssnikamall@gmail.com>
2019-03-08 12:20:43 +0100

    last test

---
+++ test_v/src/test_v/HelloWorld.java
@@ -0,0 +1,12 @@
+package test_v;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helloworld");
+		System.out.println("helloworld2");
+		System.out.println("helloworld3");
+	}
+
+}
